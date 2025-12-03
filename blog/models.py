207915afa3ec786cc97b377a3a7148c59b9fcca0
dmitrii215 <dimitry8st@gmail.com>
2025-12-03 19:43:48 +0300

    feat(posts): создал модель поста и зарегистрировал в админке

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name = 'пост'
+        verbose_name_plural = 'Посты'
+        db_table = 'blog_posts'
+
+    def __str__(self):
+        return self.title
+
+
+
